6ef13386f6a01e8180fb34445b32b6f4bb66bd8a
Roz Bryant Dupiton <rozbryant@gmail.com>
2016-10-24 22:34:52 -0500

    /scissors functional

--- app/controllers/scissors_controller.rb
+++ app/controllers/scissors_controller.rb
@@ -14,16 +14,6 @@ class ScissorsController < ApplicationController
 
     @user_move = path_as_move
 
-    if @user_move == @computer_move
-      puts "You're tied!!"
-    elsif @user_move == "scissors" && @computer_move == "rock"
-      puts "You lose!!"
-    elsif @user_move == "scissors" && @computer_move == "paper"
-      puts "You win!!"
-    end
-
     render("/play_scissors.html.erb")
   end
 end
-
-  
